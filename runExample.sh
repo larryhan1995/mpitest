c167da3a6cb925c16c54b2886526cad2d031fadb
Han Lu <43052004+larryhan1995@users.noreply.github.com>
2021-10-28 11:29:20 -0500

    Update runExample.sh

--- runExample.sh
+++ runExample.sh
@@ -1,2 +1,11 @@
 cd /home/azureuser/lwd/test/3layer
+echo "check files. File list:"
+ls
+echo "check algo_config:"
+cat algo_config.dat
+echo "check boundary.dat:"
+cat boundary.dat
+echo "check prior:"
+cat prior_params.dat
+cat prior_types.dat
 mpirun -np 1 ../../src/tmcmc_lwd 
