92bc13653b1028eec5fc8e4f178683408f732454
Han Lu <43052004+larryhan1995@users.noreply.github.com>
2021-10-28 11:01:01 -0500

    Update findFiles.sh

--- findFiles.sh
+++ findFiles.sh
@@ -1,6 +1,2 @@
-echo "find TMCMC lib"
-find / -name '*TMCMC*'
-echo "find forward lib"
-find / -name '*dipole*'
-echo "find app folder"
-find / -name '*3layer*'
+echo "find app"
+find /home/azureuser/lwd/ -name 'tmcmc_lwd'
