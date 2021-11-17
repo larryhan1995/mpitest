9eb150d3299c4758b67057707f2e655a8c03a560
Han Lu <43052004+larryhan1995@users.noreply.github.com>
2021-11-17 13:00:46 -0600

    Update runExample.sh

--- runExample.sh
+++ runExample.sh
@@ -4,15 +4,17 @@ echo "cp work dir"
 cp -r /home/azureuser/lwd/test/3layer ./
 cd 3layer
 rm algo_config.dat
-wget http://github.com/larryhan1995/mpitest/raw/main/inputFiles/algo_config.dat ./algo_config.dat
+wget http://github.com/larryhan1995/mpitest/raw/main/inputFiles/algo_config_1.dat ./
+wget http://github.com/larryhan1995/mpitest/raw/main/inputFiles/algo_config_2.dat ./
 echo "current location"
 pwd
 echo "file list:"
 ls
+mv algo_config_1.dat algo_config.dat
 echo "current configurations:"
 cat algo_config.dat
 echo "check PATH: $PATH"
 export PATH=$PATH:/home/azureuser/lwd/src/
 echo "check PATH after: $PATH"
-echo "run 4 process"
-mpirun -np 4 tmcmc_lwd
+echo "run 8 process"
+mpirun -np 8 tmcmc_lwd
