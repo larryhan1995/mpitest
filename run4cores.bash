ff286b95a328350cd1b0d51f94e9de1bd7076ebf
Han Lu <43052004+larryhan1995@users.noreply.github.com>
2021-11-19 11:08:02 -0600

    Create run4cores.bash

---
+++ run4cores.bash
@@ -0,0 +1,19 @@
+echo "current location"
+pwd
+echo "cp work dir"
+cp -r /home/azureuser/lwd/test/3layer ./
+cd 3layer
+rm algo_config.dat
+wget http://github.com/larryhan1995/mpitest/raw/main/inputFiles/algo_config_2.dat ./
+echo "current location"
+pwd
+echo "file list:"
+ls
+mv algo_config_2.dat algo_config.dat
+echo "current configurations:"
+cat algo_config.dat
+echo "check PATH: $PATH"
+export PATH=$PATH:/home/azureuser/lwd/src/
+echo "check PATH after: $PATH"
+echo "run 4 process"
+mpirun -np 4 tmcmc_lwd
